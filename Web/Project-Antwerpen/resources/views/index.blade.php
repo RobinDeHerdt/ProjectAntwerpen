d0953ba2389c140a8af4e53d13e1eaeb17d106b3
LukaVerhoeven <lka.v.lv@gmail.com>
2016-04-22 15:39:50 +0200

    index page en css

---
+++ Web/Project-Antwerpen/resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Antwerpen Homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../public/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../../public/css/style.css">
+
+    </script>
+  </head>
+  <body>
+    <div class="navbar ">
+      <div class="">
+
+        <div class="navbar-header">
+          <a class="navbar-left" href="#"><img src="../../public/img/A_logo_RGB_123x123.jpg" alt="Logo antwerpen"/></a>
+          <a class="navbar-brand titelpage" href="#">Antwerpen Projecten</a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+       </ul>
+
+      </div>
+    </div>
+
+
+    <div class="container">
+
+
+    <div class="row">
+
+
+    <div class="col-md-1">
+texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
+    </div>
+</div>
+    </div>
+  </body>
+</html>
