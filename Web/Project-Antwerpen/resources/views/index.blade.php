4db6e0b563e9400bdb5e0276f34a6d240206fbb6
LukaVerhoeven <lka.v.lv@gmail.com>
2016-04-24 14:32:00 +0200

    Header Homepage

--- Web/Project-Antwerpen/resources/views/index.blade.php
+++ Web/Project-Antwerpen/resources/views/index.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../public/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="../../public/css/style.css">
+    <link rel="stylesheet" href="../../public/css/font-awesome.min.css">
 
     </script>
   </head>
@@ -16,28 +17,18 @@
 
         <div class="navbar-header">
           <a class="navbar-left" href="#"><img src="../../public/img/A_logo_RGB_123x123.jpg" alt="Logo antwerpen"/></a>
-          <a class="navbar-brand titelpage" href="#">Antwerpen Projecten</a>
+          <a class="navbar-left titelpage" href="#">Antwerpen Projecten</a>
         </div>
 
         <ul class="nav navbar-nav navbar-right">
-         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+         <li><a href="#"><span class="fa  fa-pencil "></span> Sign Up</a></li>
+         <li><a href="#"><span class="fa  fa-sign-in "></span> Login</a></li>
        </ul>
 
       </div>
     </div>
 
 
-    <div class="container">
 
-
-    <div class="row">
-
-
-    <div class="col-md-1">
-texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext
-    </div>
-</div>
-    </div>
   </body>
 </html>
