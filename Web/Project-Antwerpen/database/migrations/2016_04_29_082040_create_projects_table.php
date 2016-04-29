beeac8a160c30a642aa25d4fac298d3129f28fd8
Robin De Herdt <rdh_robin@hotmail.com>
2016-04-29 11:59:44 +0200

    Database migrations added

---
+++ Web/Project-Antwerpen/database/migrations/2016_04_29_082040_create_projects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('project_name');
+            $table->string('headerimage');
+            $table->string('info');
+            $table->string('location');
+            $table->string('postalcode');
+            $table->integer('xcoord');
+            $table->integer('ycoord');
+            $table->string('thema');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
