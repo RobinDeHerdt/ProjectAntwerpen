fe3efa64d7659b66c16109772a95739c09d8000e
LukaVerhoeven <lka.v.lv@gmail.com>
2016-04-30 20:33:08 +0200

    PageLoad SortOnDate

--- Web/Project-Antwerpen/public/js/sort.js
+++ Web/Project-Antwerpen/public/js/sort.js
@@ -1,4 +1,9 @@
+var $wrapper = $('.sortwrapper');
 
+$wrapper.find('.sort').sort(function (a, b) {
+    return +a.dataset.date - +b.dataset.date;
+})
+.appendTo( $wrapper );
 
 $('#sort1').on('click', function () {
   var $wrapper = $('.sortwrapper');
