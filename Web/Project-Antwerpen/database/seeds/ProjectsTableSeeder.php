672643a7a06baf79d2118e92956f312be9ee09f4
LukaVerhoeven <lka.v.lv@gmail.com>
2016-05-02 20:43:06 +0200

    HoomepageAlmostDone

--- Web/Project-Antwerpen/database/seeds/ProjectsTableSeeder.php
+++ Web/Project-Antwerpen/database/seeds/ProjectsTableSeeder.php
@@ -14,7 +14,7 @@ class ProjectsTableSeeder extends Seeder
 
         DB::table('projects')->insert([
         	'project_name' => 'Nieuwe sporen centraal station',
-        	'headerimage' => '../img/station.jpg',
+        	'headerimage' => '/img/station.jpg',
         	'info' => 'Er komen 3 nieuwe sporen in het centraal station',
         	'location' => 'Antwerpen',
         	'postalcode' => '2000',
@@ -28,7 +28,7 @@ class ProjectsTableSeeder extends Seeder
 
     	DB::table('projects')->insert([
         	'project_name' => 'Vernieuwing MAS',
-        	'headerimage' => '../img/mas.jpg',
+        	'headerimage' => '/img/mas.jpg',
         	'info' => 'Het Museum aan de Stroom zal vernieuwd worden.',
         	'location' => 'Antwerpen',
         	'postalcode' => '2000',
@@ -37,8 +37,7 @@ class ProjectsTableSeeder extends Seeder
         	'thema' => 'Architectuur',
         	'start_date' => '2016-06-21',
         	'end_date' => '2016-12-',
-        	'color' => 'yellow',
+        	'color' => 'green',
     	]);
     }
 }
-
