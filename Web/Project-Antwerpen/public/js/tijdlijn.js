a1525ad6143f956fce37017448c613783eb455c8
LukaVerhoeven <lka.v.lv@gmail.com>
2016-05-13 12:10:15 +0200

    sorteerMilstones

--- Web/Project-Antwerpen/public/js/tijdlijn.js
+++ Web/Project-Antwerpen/public/js/tijdlijn.js
@@ -4,7 +4,7 @@
      $interpolateProvider.endSymbol('%>');
   });
 
-  app.controller('FasenController', function(){
+  app.controller('FasenController', function($filter){
     this.Fasen = Milestonefases;
 
     this.deletemilestone =function(obj) {
@@ -28,8 +28,20 @@
     this.fases;
 
     this.MilestoneToJson = function () {
+
+
+      Milestonefases.sort(function(a,b){
+        var date1 = $filter('date')(new Date(a["startdate"]), 'yyyy-MM-dd');
+        date1 = date1.split('-');
+        date1 = date1[0] +date1[1] -1 + date1[2];
+        var date2  = $filter('date')(new Date(b["startdate"]), 'yyyy-MM-dd')
+        date2= date2.split('-');
+        date2= date2[0] +date2[1] -1 + date2[2];
+        return date1 > date2 ? 1 : -1;
+      })
+
       this.fases = JSON.stringify(Milestonefases);
-    }
+    };
 
 
   });
@@ -42,12 +54,13 @@
       title: 'Opbouw',
 
 
+
     }, {
       title: 'Verbouwings werken',
 
     }, {
       title: 'Nazorg',
-
+      
     }
   ];
 
