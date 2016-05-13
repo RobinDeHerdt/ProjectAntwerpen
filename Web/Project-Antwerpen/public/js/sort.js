aa7cb83c5637363a9ce4752d6ad654841b930ce8
LukaVerhoeven <lka.v.lv@gmail.com>
2016-05-13 14:34:51 +0200

    buttonfixes

--- Web/Project-Antwerpen/public/js/sort.js
+++ Web/Project-Antwerpen/public/js/sort.js
@@ -24,8 +24,6 @@ function sortByDate() {
     }).each(function(){
         $wrapper.prepend(this);
     });
-
-
 }
 
 
@@ -38,21 +36,3 @@ $('#sort').on('click', function () {
   })
   .appendTo( $wrapper );
 });
-//show & hide form --> project aanmaken --> milestone toevoegen
-$("#addMilestone").hide();
-$("#milestoneButtonTogle div:first-child").hide();
-function TogleForm() {
-  $("#addMilestone").toggle();
-  $("#btn-button-milestone" ).toggleClass( "btn-success" );
-  $("#btn-button-milestone" ).toggleClass( "btn-info" );
-  $("#btn-button-milestone").prop('value') == "Formulier minimaliseren" ? $("#btn-button-milestone").prop('value', 'Mijlpaal Toevoegen') : $("#btn-button-milestone").prop('value', 'Formulier minimaliseren');
-  if ($("#milestoneButtonTogle div:first-child").is(":visible")) {
-    $("#milestoneButtonTogle div:first-child").hide();
-    $("#milestoneButtonTogle div:last-child").show();
-  }else{
-    $("#milestoneButtonTogle div:first-child").show();
-    $("#milestoneButtonTogle div:last-child").hide();
-  }
-}
-$("#btn-button-milestone").click(function() {  TogleForm() });
-$('#milestoneButtonTogle div:first-child').on('click', function(){  TogleForm() });
