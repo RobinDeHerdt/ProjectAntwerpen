3dbfc24d774d95b3ccd1ef97b4df4c102e648883
LukaVerhoeven <lka.v.lv@gmail.com>
2016-05-25 10:07:24 +0200

    Graph data uit de database halen

--- Web/Project-Antwerpen/resources/views/pages/project-meningen.blade.php
+++ Web/Project-Antwerpen/resources/views/pages/project-meningen.blade.php
@@ -7,17 +7,21 @@
 @section('content')
 <link rel="stylesheet" href="/css/donut-graph.css">
 
-<h1>Dit is een mening MeningVraag?</h1>
+<h1 id="meningvraag"></h1>
 
 <div class="chart-wrapper">
   <button onclick='replay()' class="prevbutton" type="button" name="button">Prev</button>
-<button onclick='replay()' class="nextbutton" type="button" name="button">Next</button>
+<button onclick='replay()' class="nextbutton" type="button" name="button" href="questions_json">Next</button>
+<input type="button" id="Graphdata" name="Graphdata" value="{{json_encode($questions)}}">
+
+
 </div>
 
 
 
 
 <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+<script src="/js/unserialise.js" ></script>
 <script src="/js/donutgraph.js" ></script>
 
 
