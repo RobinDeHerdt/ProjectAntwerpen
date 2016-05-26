0447623c8faec11d78df151bfef7156adfd3a264
LukaVerhoeven <lka.v.lv@gmail.com>
2016-05-26 18:54:33 +0200

    DeleteMeningVragen

---
+++ Web/Project-Antwerpen/resources/views/pages/deleteopinionquestion.blade.php
@@ -0,0 +1,45 @@
+@extends('layout')
+
+ @section('title')
+   Nieuwe meningvraag
+ @stop
+
+ @section('content')
+ <link rel="stylesheet" href="/css/donut-graph.css">
+ <div class="col-md-offset-3 col-md-6">
+   <div ng-app="questionapp">
+     <div ng-controller="questionController as Qcont">
+     	<form method="post" action="" class="OpninionQuestionFrom">
+
+    		{!! csrf_field() !!}
+    	    <label for="Project_names">Kies u project: </label>
+          <select ng-model="projects"class="c-select form-control input-md"  ng-click="Qcont.updateid()" id="Project_names" name="Project_names" alt="Kies een project waaraan u een mening wilt toevoegen" value="{{old('project->projectname')}}">
+
+          <option ng-repeat="projectname in Qcont.projectnames" data="<% projectname.id %>" alt="Project: <% projectname.name %>" ><% projectname.name %></option>
+
+
+    	    </select>
+          <div ng-repeat="projectquestion in Qcont.projectquestions" ng-if="projectquestion.id == Qcont.projectID">
+            <li  data="<%projectquestion.id%>"><%projectquestion.body%></li>
+            <a href="deleteopinionquestion/<%projectquestion.id%>" class="btn btn-danger">delete</a>
+
+          </div>
+
+
+
+
+
+
+              <input type="hidden" id="hiddenprojects" name="name" value="{{$projects}}">
+              <input type="hidden" id="hiddenquestions" name="name" value="{{$questions}}">
+
+        </div>
+
+      </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+    <script src="/js/deleteopinionquestion.js" ></script>
+	</form>
+</div>
+@stop
