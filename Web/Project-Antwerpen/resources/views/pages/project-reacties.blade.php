95a0cc453a145e1b4420013c7a8c3537292d1ffc
DieterVercammen <dietervercammen@gmail.com>
2016-05-27 15:27:05 +0200

    css fixes

--- Web/Project-Antwerpen/resources/views/pages/project-reacties.blade.php
+++ Web/Project-Antwerpen/resources/views/pages/project-reacties.blade.php
@@ -57,7 +57,7 @@
 		@endforeach
 
 
-		<div class=" col-md-11" style="margin-left:5%">
+		<div class=" col-md-12 reactie-error">
 		@if (Auth::guest())
 			<strong>Je moet ingelogd zijn om een reactie te geven. Je kan <a href="\login">hier</a> inloggen.</strong>
 			<br />
