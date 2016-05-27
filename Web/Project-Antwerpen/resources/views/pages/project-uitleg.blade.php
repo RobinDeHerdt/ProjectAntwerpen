304224983f2296c7ee22fe4b64ee7d4af13a1748
DieterVercammen <dietervercammen@gmail.com>
2016-05-27 15:50:51 +0200

    info page kleine styling

--- Web/Project-Antwerpen/resources/views/pages/project-uitleg.blade.php
+++ Web/Project-Antwerpen/resources/views/pages/project-uitleg.blade.php
@@ -25,9 +25,14 @@
     color: #7f8c97;
     font-size: 15px;
     }
+    .fase-text {
+        margin-top:50px;
+    }
     
     
-    .bg-fase:nth-child(even) {background-color: #b0b0b0;}
+    .bg-fase:nth-child(even) {background-color: #DBDBDB;
+        margin-left: -381px;
+    margin-right: -381px;}
 
     .imgtile>img {width:65%; margin:3em;}
 
@@ -45,7 +50,7 @@
     margin-left: -50px;
     width: 0;
     height: 0;
-    border-top: solid 50px #b0b0b0;
+    border-top: solid 50px #DBDBDB;
     border-left: solid 50px transparent;
     border-right: solid 50px transparent;
 }
@@ -63,17 +68,21 @@
     
      display: none;;
 }
+.project-info {
+    min-height:50vh;
+}
 
 </style> 
 <div class="container">
 
     <div class="row ">
-        <div class="col-md-12" style="text-align: center;">
+        <div class="col-md-12 project-info" style="text-align: center;">
             <h2>Projectinformatie</h2>
-            <h5>Begindatum: {{  $project->start_date    }}</h5>
-            <h5>Einddatum:  {{  $project->end_date      }}</h5>
-            <h5>Locatie:    {{  $project->location      }}</h5>
-            <h5>Postcode:   {{  $project->postalcode    }}</h5>
+            <h5><i class="fa fa-calendar-check-o"> </i>Begindatum: {{  $project->start_date    }}</h5>
+            <h5><i class="fa fa-calendar-times-o"> </i>Einddatum:  {{  $project->end_date      }}</h5>
+            <h5><i class="fa fa-map-marker"> </i>Locatie:    {{  $project->location      }}</h5>
+            <h5><i class="fa fa-location-arrow"></i> 
+Postcode:   {{  $project->postalcode    }}</h5>
 
             <br>
             
@@ -84,7 +93,7 @@
     @foreach($projectfases as $key=>$fase)
         <div class="row  bg-fase" id="fase{{$key + 1}}">
             <!-- <div class="col-md-4 imgtile Tile3"> -->
-                <div class="col-md-12">
+                <div class="col-md-12 fase-text">
                     <h2>Fase {{ $key + 1 }}</h2>
                     <h3>{{ $fase->milestone_title}}</h3>
                     <p>{{ $fase->milestone_info }}</p>
