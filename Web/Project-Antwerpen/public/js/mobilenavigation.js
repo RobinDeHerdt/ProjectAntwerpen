81ac4bf71739af78f89e00706d50d544126ca809
LukaVerhoeven <lka.v.lv@gmail.com>
2016-05-27 15:48:20 +0200

    navfix

--- Web/Project-Antwerpen/public/js/mobilenavigation.js
+++ Web/Project-Antwerpen/public/js/mobilenavigation.js
@@ -1,6 +1,4 @@
 $(document).ready ( function () {
-  var currentpage = 0;
-  var allprojectpages = ["tijdlijn", "info", "kaart", "meningen","reacties" ]
     $(document).on ("click", "#navmobile", function () {
           $( "#navbar li" ).toggle( "slow" );
           $('#navmobile').toggleClass("img2");
