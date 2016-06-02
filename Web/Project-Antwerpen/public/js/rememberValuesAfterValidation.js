c1098abec57a8b0fe64c877a40aa3bbf69afc0ce
Robin De Herdt <rdh_robin@hotmail.com>
2016-06-02 16:18:56 +0200

    Inline js verplaatst

---
+++ Web/Project-Antwerpen/public/js/rememberValuesAfterValidation.js
@@ -0,0 +1,29 @@
+if(document.getElementById("saveName").value) 
+{
+    document.getElementById("project_name").value = document.getElementById("saveName").value;
+}
+
+if(document.getElementById("saveInfo").value)
+{
+    document.getElementById("project_info").value = document.getElementById("saveInfo").value;
+} 
+
+if(document.getElementById("savePostalcode").value)
+{
+    document.getElementById("project_postalcode").value = document.getElementById("savePostalcode").value;
+}
+
+if(document.getElementById("saveLocation").value)
+{
+    document.getElementById("project_location").value = document.getElementById("saveLocation").value;
+}
+
+if(document.getElementById("saveStartdate").value)
+{
+    document.getElementById("project_startdate").value = document.getElementById("saveStartdate").value;
+}
+
+if(document.getElementById("saveEnddate").value)
+{
+    document.getElementById("project_enddate").value = document.getElementById("saveEnddate").value;
+}
